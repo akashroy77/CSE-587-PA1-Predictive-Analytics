696ed82016411538d0e8044a0623e260e2ca300b
Diksha Marathe <dikshamarathe@gmail.com>
2020-03-09 18:27:36 -0400

    added KNN and Kmeans

--- Predictive_Analytics.py
+++ Predictive_Analytics.py
@@ -18,6 +18,7 @@ from sklearn.svm import SVC
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.linear_model import LogisticRegression
 from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import VotingClassifier
 
 df = pd.read_csv("D:/UB Courses/cse587/Assignment1/data.csv", delimiter = ",")
 
@@ -31,6 +32,11 @@ X = df_normalized.to_numpy()
 x_train,x_test,y_train,y_test=train_test_split(X,y,test_size=0.2)
 
 no_of_classes = 11
+
+knn = KNeighborsClassifier()
+clf = SVC()
+log_reg = LogisticRegression()
+decision_tree = DecisionTreeClassifier()
 
 def Accuracy(y_true,y_pred):
     """
@@ -67,24 +73,42 @@ def confusion_matrix(y_pred, y_test, no_of_classes):
 
 
 def plot_(y_pred, y_test, no_of_classes):
-    sns.set(rc={'figure.figsize':(6,5)})
-    ax = sns.heatmap(confusion_matrix(y_pred, y_test, no_of_classes), cmap='YlGnBu', annot=True,fmt='d')
-    bottom,top = ax.get_ylim()
-    ax.set_ylim(bottom+0.5,top - 0.5)
-    # plt.imshow(final_conf)
+    data = confusion_matrix(y_pred, y_test,11)
+    plt.imshow(data,cmap='Wistia')
+    for x in range(data.shape[0]):
+        for y in range(data.shape[1]):
+            plt.text(x,y,data[x, y],horizontalalignment='center',verticalalignment='center')
     plt.xlabel('predicted')
     plt.ylabel('actual')
+    plt.grid(False)
+    plt.colorbar()
     plt.show()
 
 
+'''KNN Algorithm implementation'''
+
+def calculate_distance_matrix(A,b,Y_train):
+    d = np.linalg.norm(A - b, axis = 1)
+    d = np.column_stack((d, Y_train))
+    d = sorted(d, key =(lambda x: x[0]))
+    d = np.asarray(d)
+    return d
+
 def KNN(X_train,X_test,Y_train):
-     """
-    :type X_train: numpy.ndarray
-    :type X_test: numpy.ndarray
-    :type Y_train: numpy.ndarray
-    
-    :rtype: numpy.ndarray
-    """
+    predicted_labels = []
+    for x_t in X_test:
+        distance_matrix = calculate_distance_matrix(X_train, x_t, Y_train)
+        distance_matrix = distance_matrix[0:k]
+        test_labels = distance_matrix[::, -1]
+        test_labels = test_labels.astype("int64")
+        counts = np.bincount(test_labels)
+        label = np.argmax(counts)
+        predicted_labels.append(label)
+    predicted_labels = np.asarray(predicted_labels)
+    return predicted_labels
+
+''' End of KNN Classifier Algorithm'''
+
 def RandomForest(X_train,Y_train,X_test):
     """
     :type X_train: numpy.ndarray
@@ -121,31 +145,64 @@ def PCA(x_train, N):
     return sample_data
 
     
-def Kmeans(X_train,N):
-    """
-    :type X_train: numpy.ndarray
-    :type N: int
-    :rtype: List[numpy.ndarray]
-    """
+''' K-Means Clustering Algorithm Implementation'''
 
-def SklearnSupervisedLearning(X_train,Y_train,X_test):
-    """
-    :type X_train: numpy.ndarray
-    :type X_test: numpy.ndarray
-    :type Y_train: numpy.ndarray
-    
-    :rtype: List[numpy.ndarray] 
-    """
+def create_empty_clusters(k):
+    clusters = []
+    for i in range(k):
+        clusters.append([])
 
-def SklearnVotingClassifier(X_train,Y_train,X_test):
+    return clusters
+
+def assigning_clusters(X,cluster_centers):
+    clusters  = create_empty_clusters(cluster_centers.shape[0])
+    for i in range(len(X)):
+        dist = np.linalg.norm(X[i] - cluster_centers, axis = 1)
+        dist = np.asarray(dist)
+        min_dist_index = np.argmin(dist)
+        clusters[min_dist_index].append(X[i])
+    clusters = np.asarray(clusters)
+    return clusters
+
+
+def Kmeans(X_train, N):
+    X = X_train
+    k = N
+
+    cluster_centers_old = np.zeros((k, X.shape[1]))
+    cluster_centers = X[np.random.randint(X.shape[0], size=k)] #Randomly selecting data points to be initial cluster centroids
+    error = np.sum(np.linalg.norm(cluster_centers - cluster_centers_old, axis=1))
+    while error != 0:
+        clusters = assigning_clusters(X, cluster_centers)
+        cluster_centers_old = deepcopy(cluster_centers)
+        for i, row in enumerate(clusters):
+            if len(row) == 0:
+                continue
+            cluster_centers[i] = np.mean(row)
+        error = np.sum(np.linalg.norm(cluster_centers - cluster_centers_old, axis=1))
+    return clusters
+
+
+''' End of K-means clustering algorithm'''
+
+def SklearnSupervisedLearning(x_train,y_train,x_test):
+    knn_(x_train, x_test, 5)
+    knn_grid(x_train, x_test, no_of_classes)
+    SVM_(x_train, x_test, y_test)
+    SVM_grid(x_train, x_test, y_test)
+    logReg(x_train, x_test, y_test)
+    logReg_grid(x_train, x_test, y_test)
+    DT(x_train, x_test, y_test)
+    DT_grid(x_train, x_test, y_test)
     
-    """
-    :type X_train: numpy.ndarray
-    :type X_test: numpy.ndarray
-    :type Y_train: numpy.ndarray
-    
-    :rtype: List[numpy.ndarray] 
-    """
+
+def SklearnVotingClassifier(x_train,y_train,x_test):
+    estimators=[('knn', knn), ('svm', clf), ('DT',decision_tree), ('log_reg', log_reg)]
+    ensemble = VotingClassifier(estimators, voting='hard')
+    ensemble.fit(x_train, y_train)
+    print(ensemble.score(x_test, y_test))
+    y_pred = ensemble.predict(x_test)
+    return y_pred
 
 def knn_(x_train, x_test, N):
     knn = KNeighborsClassifier(n_neighbors = 5)
@@ -153,6 +210,7 @@ def knn_(x_train, x_test, N):
     y_pred = knn.predict(x_test)
     print(metrics.accuracy_score(y_test, y_pred))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
     
 
 def knn_grid(x_train, x_test, N):
@@ -165,6 +223,7 @@ def knn_grid(x_train, x_test, N):
     y_pred = knn_gs.predict(x_test)
     print('knn: {}'.format(knn_gs.score(x_test, y_test)))
     plot_(y_pred, y_test, N)
+    return y_pred
     
 
 def SVM_(x_train, x_test, y_test):
@@ -181,6 +240,7 @@ def SVM_(x_train, x_test, y_test):
     
     print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
     
 
 def SVM_grid(x_train, x_test, y_test):
@@ -195,6 +255,7 @@ def SVM_grid(x_train, x_test, y_test):
     
     print("Accuracy:",metrics.accuracy_score(y_test, y_pred))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
 
     
 def logReg(x_train, x_test, y_test):
@@ -214,6 +275,7 @@ def logReg(x_train, x_test, y_test):
     
     print('log_reg: {}'.format(log_reg.score(x_test, y_test)))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
     
     
 def logReg_grid(x_train, x_test, y_test):
@@ -223,6 +285,7 @@ def logReg_grid(x_train, x_test, y_test):
     y_pred = log.predict(x_test)
     print('log_reg_grid: {}'.format(log.score(x_test, y_test)))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
     
 
 def DT(x_train, x_test, y_test):
@@ -231,6 +294,7 @@ def DT(x_train, x_test, y_test):
     y_pred = decision_tree.predict(x_test)
     print('DT: {}'.format(decision_tree.score(x_test, y_test)))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
 
     
 def DT_grid(x_train, x_test, y_test):
@@ -241,16 +305,16 @@ def DT_grid(x_train, x_test, y_test):
     y_pred = dt_grid.predict(x_test)
     print('DT_grid: {}'.format(dt_grid.score(x_test, y_test)))
     plot_(y_pred, y_test, no_of_classes)
+    return y_pred
 """
 Create your own custom functions for Matplotlib visualization of hyperparameter search. 
 Make sure that plots are labeled and proper legends are used
 """
-
-"""#https://towardsdatascience.com/knn-using-scikit-learn-c6bed765be75
+#https://towardsdatascience.com/knn-using-scikit-learn-c6bed765be75
 #https://towardsdatascience.com/ensemble-learning-using-scikit-learn-85c4531ff86a
 #https://www.datacamp.com/community/tutorials/svm-classification-scikit-learn-python
 #https://datascience.stackexchange.com/questions/989/svm-using-scikit-learn-runs-endlessly-and-never-completes-execution
 #https://www.geeksforgeeks.org/svm-hyperparameter-tuning-using-gridsearchcv-ml/
 #https://towardsdatascience.com/ensemble-learning-using-scikit-learn-85c4531ff86a
 #https://chrisalbon.com/machine_learning/model_selection/hyperparameter_tuning_using_grid_search/
-#https://scikit-learn.org/stable/modules/tree.html"""
+#https://scikit-learn.org/stable/modules/tree.html
